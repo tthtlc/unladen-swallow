c2c26f78322d4a0717cf00ffc3e40d20b1378400
jyasskin <jyasskin@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2008-12-29 17:27:39 +0000

    r53@jyasskin-macbookpro15:  jyasskin | 2008-12-26 11:17:13 -0700  Fix pyexpat to not rely on the type of the instruction list argument to  PyCode_New().

--- branches/vmgen/Objects/codeobject.c
+++ branches/vmgen/Objects/codeobject.c
@@ -108,6 +108,58 @@ PyCode_New(int argcount, int nlocals, int stacksize, int flags,
 	return co;
 }
 
+PyCodeObject *
+PyCode_NewEmpty(const char *filename, const char *funcname, int firstlineno)
+{
+	static PyObject *instructions = NULL;
+	static PyObject *nulltuple = NULL;
+	static PyObject *lnotab = NULL;
+	PyObject *filename_ob = NULL;
+	PyObject *funcname_ob = NULL;
+	PyCodeObject *result = NULL;
+	if (instructions == NULL) {
+		instructions = (PyObject *)_PyInstructions_New(0);
+		if (instructions == NULL)
+			goto failed;
+	}
+	if (nulltuple == NULL) {
+		nulltuple = PyTuple_New(0);
+		if (nulltuple == NULL)
+			goto failed;
+	}
+	if (lnotab == NULL) {
+		lnotab = PyString_FromString("");
+		if (lnotab == NULL)
+			goto failed;
+	}
+	funcname_ob = PyString_FromString(funcname);
+	if (funcname_ob == NULL)
+		goto failed;
+	filename_ob = PyString_FromString(filename);
+	if (filename_ob == NULL)
+		goto failed;
+
+	result = PyCode_New(0,			/* argcount */
+			    0,			/* nlocals */
+			    0,			/* stacksize */
+			    0,			/* flags */
+			    instructions,	/* code */
+			    nulltuple,		/* consts */
+			    nulltuple,		/* names */
+			    nulltuple,		/* varnames */
+			    nulltuple,		/* freevars */
+			    nulltuple,		/* cellvars */
+			    filename_ob,	/* filename */
+			    funcname_ob,	/* name */
+			    firstlineno,	/* firstlineno */
+			    lnotab		/* lnotab */
+			    );
+
+failed:
+	Py_XDECREF(funcname_ob);
+	Py_XDECREF(filename_ob);
+	return result;
+}
 
 #define OFF(x) offsetof(PyCodeObject, x)
 
