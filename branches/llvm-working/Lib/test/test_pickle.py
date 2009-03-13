d677d9b90efcd602d44874200063a683ea9e28af
jyasskin <jyasskin@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-03-13 21:36:51 +0000

    Make a review-less working branch for the LLVM changes so I don't block on reviews. As I get changes reviewed and merged into trunk, I'll port any review comments back here.

---
+++ branches/llvm-working/Lib/test/test_pickle.py
@@ -0,0 +1,86 @@
+import pickle
+from cStringIO import StringIO
+
+from test import test_support
+
+from test.pickletester import AbstractPickleTests
+from test.pickletester import AbstractPickleModuleTests
+from test.pickletester import AbstractPersistentPicklerTests
+from test.pickletester import AbstractPicklerObjectTests
+from test.pickletester import AbstractUnpicklerObjectTests
+
+class PickleTests(AbstractPickleTests, AbstractPickleModuleTests):
+
+    def dumps(self, arg, proto=0, fast=0):
+        # Ignore fast
+        return pickle.dumps(arg, proto)
+
+    def loads(self, buf):
+        # Ignore fast
+        return pickle.loads(buf)
+
+    module = pickle
+    error = KeyError
+
+class PicklerTests(AbstractPickleTests):
+
+    error = KeyError
+
+    def dumps(self, arg, proto=0, fast=0):
+        f = StringIO()
+        p = pickle.Pickler(f, proto)
+        if fast:
+            p.fast = fast
+        p.dump(arg)
+        f.seek(0)
+        return f.read()
+
+    def loads(self, buf):
+        f = StringIO(buf)
+        u = pickle.Unpickler(f)
+        return u.load()
+
+class PersPicklerTests(AbstractPersistentPicklerTests):
+
+    def dumps(self, arg, proto=0, fast=0):
+        class PersPickler(pickle.Pickler):
+            def persistent_id(subself, obj):
+                return self.persistent_id(obj)
+        f = StringIO()
+        p = PersPickler(f, proto)
+        if fast:
+            p.fast = fast
+        p.dump(arg)
+        f.seek(0)
+        return f.read()
+
+    def loads(self, buf):
+        class PersUnpickler(pickle.Unpickler):
+            def persistent_load(subself, obj):
+                return self.persistent_load(obj)
+        f = StringIO(buf)
+        u = PersUnpickler(f)
+        return u.load()
+
+class PicklerObjectTests(AbstractPicklerObjectTests):
+
+    pickler_class = pickle.Pickler
+
+class UnpicklerObjectTests(AbstractUnpicklerObjectTests):
+
+    pickler_class = pickle.Pickler
+    unpickler_class = pickle.Unpickler
+
+
+def test_main():
+    test_support.run_unittest(
+        PickleTests,
+        PicklerTests,
+        PersPicklerTests,
+        PicklerObjectTests,
+        UnpicklerObjectTests,
+    )
+    test_support.run_doctest(pickle)
+
+if __name__ == "__main__":
+    test_main()
