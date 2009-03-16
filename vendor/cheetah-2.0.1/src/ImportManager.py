bb9d2b0cc0dad3c3bb42727a287b768bf68b7c52
collinw <collinw@05521daa-c0b4-11dd-bb00-bd6ab96fe29a>
2009-03-16 18:00:31 +0000

    Fix Cheetah's import hook to work with Python 2.6.

--- vendor/cheetah-2.0.1/src/ImportManager.py
+++ vendor/cheetah-2.0.1/src/ImportManager.py
@@ -407,7 +407,7 @@ class ImportManager:
         __builtin__.__import__ = self.importHook
         __builtin__.reload = self.reloadHook
         
-    def importHook(self, name, globals=None, locals=None, fromlist=None):
+    def importHook(self, name, globals=None, locals=None, fromlist=None, level=-1):
         # first see if we could be importing a relative name
         #print "importHook(%s, %s, locals, %s)" % (name, globals['__name__'], fromlist)
         _sys_modules_get = sys.modules.get
